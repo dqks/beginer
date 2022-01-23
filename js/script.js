c0bd561cbd1efd64c4b9115d0b65fb2e3bcb1058
Maxim Shtin <maksimshtin@gmail.com>
2022-01-23 15:11:12 +0500

    add fucntion for image, design a button and a bit move text

--- js/script.js
+++ js/script.js
@@ -2,6 +2,36 @@ const overflowFuck = document.querySelector('.overflowFuck')
 const button = document.querySelector('#goFuckButton')
 button.onclick = function() {
     console.log('пошел нахуй');
-    overflowFuck.style.display = 'flex'
+   overflowFuck.style.display = 'flex'
 };
+
+const raper = {
+    list: [
+        {
+            name: 'Dababy'
+        },
+
+        {
+            name: 'Morgenstern'
+        },
+
+    ],
+}
+
+const vegetables = ['Cucumber', 'Tomatoe', 'Potato'];
+vegetables[2] = 'Pineapple';
+vegetables[1] = 'Apple';
+vegetables[0] = 'Dolboeb';
+
+const krim = ['Ukraina'];
+krim[0] = "Nash";   
+
+//var object = document.getElementById('image');
+//object.onclick=function(){
+ //   var x = Math.floor(Math.random()*300);
+ //  var y = Math.floor(Math.random()*300);
+  //  object.style.top = x + 'px';
+  //  object.style.left = y + 'px';
+//}
+
 
